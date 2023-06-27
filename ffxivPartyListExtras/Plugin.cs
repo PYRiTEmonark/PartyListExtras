79ee004820eb8ac022d4826f8aa2a8f367eba6cd
Iris Scoffin <iris@scoffin.com>
2023-06-27 12:33:29 +0100

    Add project files.

---
+++ ffxivPartyListExtras/Plugin.cs
@@ -0,0 +1,202 @@
+using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using System.IO;
+using Dalamud.Interface.Windowing;
+using ffxivPartyListExtras.Windows;
+using Dalamud.Game.Gui;
+using Dalamud.Game.ClientState;
+using Dalamud.Game.ClientState.Party;
+using Dalamud.Game.ClientState.Objects;
+using System.Collections.Generic;
+using ImGuiScene;
+using System.Linq;
+using Dalamud.Logging;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using System.Text.Json;
+using System;
+using Dalamud.Utility;
+using static Lumina.Data.Parsing.Uld.NodeData;
+
+namespace ffxivPartyListExtras
+{
+    // number to update so the DLL actually changes: 5
+    public sealed class Plugin : IDalamudPlugin
+    {
+        public string Name => "ffxivPartyListExtras";
+        private const string CommandName = "/plx";
+
+        private bool OverlayEnabled = true;
+
+        private DalamudPluginInterface PluginInterface { get; init; }
+        private CommandManager CommandManager { get; init; }
+        public Configuration Configuration { get; init; }
+        public WindowSystem WindowSystem = new("ffxivPartyListExtras");
+        public GameGui GameGui { get; init; }
+        public ChatGui ChatGui { get; init; }
+        public ClientState ClientState { get; init; }
+        public ObjectTable ObjectTable { get; init; }
+        public PartyList PartyList { get; init; }
+
+
+        private ConfigWindow ConfigWindow { get; init; }
+        private OverlayWindow OverlayWindow { get; init; }
+        internal Dictionary<string, TextureWrap> textures = new Dictionary<string, TextureWrap>();
+        internal Dictionary<int, StatusEffectData> statusEffectData = new Dictionary<int, StatusEffectData>();
+
+        public Plugin(
+            [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
+            [RequiredVersion("1.0")] CommandManager commandManager,
+            [RequiredVersion("1.0")] GameGui gameGui,
+            [RequiredVersion("1.0")] ChatGui chatGui,
+            [RequiredVersion("1.0")] ClientState clientState,
+            [RequiredVersion("1.0")] ObjectTable objectTable,
+            [RequiredVersion("1.0")] PartyList partyList)
+        {
+            this.PluginInterface = pluginInterface;
+            this.CommandManager = commandManager;
+            this.GameGui = gameGui;
+            this.ChatGui = chatGui;
+            this.ClientState = clientState;
+            this.PartyList = partyList;
+            this.ObjectTable = objectTable;
+
+            this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+            this.Configuration.Initialize(this.PluginInterface);
+
+            ConfigWindow = new ConfigWindow(this);
+            OverlayWindow = new OverlayWindow(this);
+            OverlayWindow.IsOpen = true;
+
+            WindowSystem.AddWindow(ConfigWindow);
+            WindowSystem.AddWindow(OverlayWindow);
+
+            this.PluginInterface.UiBuilder.Draw += DrawUI;
+            this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
+
+            // Find our image files
+            var baseImagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "Icons");
+            var imageNames = Directory.GetFiles(baseImagePath, "*.png").Select(Path.GetFileName).ToArray();
+
+            // Logging cus VS refuses to copy images sometimes
+            PluginLog.Debug("Loading images from {0}", baseImagePath);
+
+            // Load images into the dict
+            foreach (var imageName in imageNames)
+            {
+                if (imageName == null) continue;
+                var imagePath = Path.Combine(baseImagePath, imageName);
+                this.textures.Add(imageName, this.PluginInterface.UiBuilder.LoadImage(imagePath));
+            }
+
+            PluginLog.Debug("Images Loaded: {0}", string.Join(',', imageNames));
+
+            // as above but for status .json files in /StatusData
+            var baseDataPath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "StatusData");
+            var dataNames = Directory.GetFiles(baseDataPath, "*.json").Select(Path.GetFileName).ToArray();
+
+            PluginLog.Debug("Loading data files from {0}", baseDataPath);
+
+            foreach (var dataName in dataNames)
+            {
+                if (dataName == null) continue;
+                var dataPath = Path.Combine(baseDataPath, dataName);
+                using (FileStream fs = File.OpenRead(dataPath))
+                {
+                    var rawData = JsonSerializer.Deserialize<List<StatusEffectData>>(fs);
+                    if (rawData == null)
+                    {
+                        PluginLog.Warning("Data file {0} didn't load - Badly formatted?");
+                        continue;
+                    }
+
+                    foreach (StatusEffectData sxd in rawData)
+                    {
+                        if (statusEffectData.ContainsKey(sxd.row_id))
+                        {
+                            PluginLog.Warning("Key {0} exists twice; accepted {1}, rejected {2}",
+                                sxd.row_id, statusEffectData[sxd.row_id].status_name, sxd.status_name);
+                            continue;
+                        }
+                        this.statusEffectData.Add(sxd.row_id, sxd);
+                    }                    
+                }
+            }
+
+            PluginLog.Debug("Data files Loaded: {0}", string.Join(',', dataNames));
+
+            // DO THIS LAST
+            // otherwise if there's an error the command gets registered
+            this.CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "toggles the overlay"
+            });
+        }
+
+        public void Dispose()
+        {
+            this.WindowSystem.RemoveAllWindows();
+            
+            ConfigWindow.Dispose();
+            OverlayWindow.Dispose();
+
+            this.CommandManager.RemoveHandler(CommandName);
+        }
+
+        private void OnCommand(string command, string args)
+        {
+            // Currently just toggle the window on slash command
+            if (args == "debug")
+            {
+                PluginLog.Information(
+                    "Missing Status Ids: {0}",
+                    string.Join("", OverlayWindow.missing_ids
+                        .Select(x => string.Format("{0} = {1}; ", x.Item1, x.Item2))));
+            }
+            else OverlayEnabled = !OverlayEnabled;
+        }
+
+        private unsafe void DrawUI()
+        {
+            this.WindowSystem.Draw();
+
+            // show overlay if open
+            AddonPartyList* apl = (AddonPartyList*)GameGui.GetAddonByName("_PartyList");
+            if (apl == null) { return; }
+            var isvis = apl->AtkUnitBase.IsVisible;
+            OverlayWindow.IsOpen = isvis && OverlayEnabled;
+        }
+
+        public void DrawConfigUI()
+        {
+            ConfigWindow.IsOpen = true;
+        }
+    }
+    internal struct StatusEffectData
+    {
+        // to be popped out and used as Mapping Key
+        public required int row_id { get; set; }
+        // should be as in game
+        public required string status_name { get; set; }
+
+        // special acts as a custom field, e.g. stance, invuln
+        public string? special { get; set; }
+
+        // Mitigation
+        public float? phys_mit { get; set; }
+        public float? magi_mit { get; set; }
+        public float? othr_mit { get; set; }
+
+        // Damage Up
+        public float? phys_up { get; set; }
+        public float? magi_up { get; set; }
+        public float? othr_up { get; set; }
+
+        // HP Regen in potency per tick
+        // TODO: anything better than that?
+        public float? regen { get; set; }
+
+    }
+
+}
+
---
+++ ffxivPartyListExtras/Windows/OverlayWindow.cs
@@ -0,0 +1,259 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Text.Json;
+using Dalamud.Game.ClientState.Objects.SubKinds;
+using Dalamud.Game.ClientState.Objects.Types;
+using Dalamud.Game.ClientState.Statuses;
+using Dalamud.Interface.Colors;
+using Dalamud.Interface.Windowing;
+using Dalamud.Logging;
+using Dalamud.Utility;
+using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using ImGuiNET;
+
+namespace ffxivPartyListExtras.Windows;
+
+public class OverlayWindow : Window, IDisposable
+{
+    private Plugin plugin;
+
+    // Used to ensure we don't duplicate the debug status info message
+    public List<Tuple<string, string>> missing_ids = new List<Tuple<string, string>>();
+
+    public unsafe OverlayWindow(Plugin plugin) : base(
+        "OverlayWindow",
+        ImGuiWindowFlags.NoDecoration | ImGuiWindowFlags.NoScrollWithMouse | ImGuiWindowFlags.NoBackground |
+        ImGuiWindowFlags.NoMouseInputs | ImGuiWindowFlags.NoNav | ImGuiWindowFlags.NoInputs | ImGuiWindowFlags.NoSavedSettings)
+    {
+        this.Size = new Vector2(0, 0);
+        this.Position = new Vector2(0, 0);
+
+        this.plugin = plugin;
+    }
+
+    public void Dispose() { }
+
+    public unsafe override void Draw()
+    {
+        // Grab some things for later
+        // TODO: move some of these idk, imgui will shit itself if you move the drawlist
+        var drawlist = ImGui.GetBackgroundDrawList();
+        AgentHUD* pl = Framework.Instance()->GetUiModule()->GetAgentModule()->GetAgentHUD();
+        var partyMemberList = (HudPartyMember*)pl->PartyMemberList;
+        var count = pl->PartyMemberCount;
+
+        var black = ImGui.ColorConvertFloat4ToU32(new Vector4(0f, 0f, 0f, 1f));
+        var clear = ImGui.ColorConvertFloat4ToU32(new Vector4(0f, 0f, 0f, 0f));
+
+        AddonPartyList* apl = (AddonPartyList*)plugin.GameGui.GetAddonByName("_PartyList");
+        if (apl == null) { return; }
+
+        var bkg_node = apl->BackgroundNineGridNode;
+        if (bkg_node == null) { return; }
+
+        var apl_node = bkg_node->AtkResNode;
+
+        // resize window
+        // Set width and height so window is to the left of the party list
+        var width = 300;
+        this.Size = new Vector2(width, apl_node.Height);
+        this.Position = new Vector2(apl_node.ScreenX - width - 10, apl_node.ScreenY);
+
+        // nullable types my beloathed
+        drawlist.AddRectFilledMultiColor((Vector2)this.Position, (Vector2)(this.Position + this.Size), clear, black, black, clear);
+
+        // drawlist.AddText(new Vector2(15, 15), ImGui.GetColorU32(ImGuiCol.Text), count.ToString()) ;
+
+        for (var i = 0; i < count; i++)
+        {
+            // Get ResNode of the JobIcon
+            var node = apl->PartyMember[i].ClassJobIcon->AtkResNode;
+            // TEMP: Offset is temporary
+            var curpos = new Vector2(node.ScreenX - 300, node.ScreenY);
+            var childpos = curpos - (Vector2)this.Position;
+
+            ImGui.SetCursorPos(childpos);
+            ImGui.BeginChild("Player {0}".Format(i));
+
+            // PlayerCharacter and BattleNPC is both BattleChara
+            // TODO: fix this so there isn't the duplication
+            PlayerCharacter pc; BattleNpc bc;
+            StatusList sl;
+            var result = plugin.ObjectTable.SearchById(partyMemberList[i].ObjectId);
+            if (result?.GetType() == typeof(PlayerCharacter))
+            {
+                pc = (PlayerCharacter)result;
+                sl = pc.StatusList;
+            }
+            else if (result?.GetType() == typeof(BattleNpc))
+            {
+                bc = (BattleNpc)result;
+                sl = bc.StatusList;
+            }
+            else {
+                //if (result == null) PluginLog.Warning("Party List member null");
+                //else PluginLog.Warning("Unexpected member of party list: {0}", result.GetType().ToString());
+                continue;
+            };
+
+            //Spin out to helper functions because long
+            DrawStatusIcons(ParseStatusList(sl));
+
+            ImGui.EndChild();
+        }
+    }
+
+    private List<StatusIcon> ParseStatusList(StatusList sl)
+    {
+        // TODO: replace this with image/label pairs
+        // this is so we can get rid of the sillyness that is RenderWithImages
+        var output = new List<StatusIcon>();
+
+        var debug = new List<Tuple<string, string>>();
+
+        List<StatusEffectData> datas = new List<StatusEffectData>();
+
+        // Gather up status data
+        foreach (var status in sl) {
+            if (status == null) continue;
+            if (plugin.statusEffectData.ContainsKey((int)status.GameData.RowId)) {
+                datas.Add(plugin.statusEffectData[(int)status.GameData.RowId]);
+            } else {
+                debug.Add(new Tuple<string, string>(status.GameData.Name.ToString(), status.GameData.RowId.ToString()));
+            }
+        }
+
+        // Read properties out of data
+        // TODO: move all definitions of IconElement out to a json file
+        string[] special_labels = {
+            "stance", "invuln", "living_dead",
+            "kardion", "kardia", "regen", //"barrier",
+            "dp_g", "dp_r"
+        };
+        StatusIcon[] special_fstrings = new StatusIcon[] {
+            new StatusIcon {FileName = "stance.png", Label = "Stance"},
+            new StatusIcon {FileName = "invuln.png", Label = "Invuln"},
+            new StatusIcon {FileName = "living_dead.png", Label = "Living Dead"},
+            new StatusIcon {FileName = "kardia.png", Label = "Sent"},
+            new StatusIcon {FileName = "kardion.png", Label = "Recv"},
+            new StatusIcon {FileName = "regen.png", Label = "Regen"},
+            //new IconElement {FileName = "barrier.png", Label = "Barrier"},
+            new StatusIcon {FileName = "dp_g.png", Label = "Sent"},
+            new StatusIcon {FileName = "dp_r.png", Label = "Recv"},
+        };
+
+        for (int i = 0; i < special_labels.Length; i++)
+        {
+            if (datas.Select(x => x.special).Contains(special_labels[i]))
+                output.Add(special_fstrings[i]);
+        }
+
+        var phys_mit = multi_sum(datas.Select(x => x.phys_mit));
+        var magi_mit = multi_sum(datas.Select(x => x.magi_mit));
+        //var othr_mit = multi_sum(datas.Select(x => x.othr_mit));
+
+        var phys_up = multi_sum(datas.Select(x => x.phys_up));
+        var magi_up = multi_sum(datas.Select(x => x.magi_up));
+        //var othr_up = multi_sum(datas.Select(x => x.othr_up));
+
+        // Mitigation
+        if (phys_mit == magi_mit && phys_mit > 0) output.Add(new StatusIcon { FileName="mit_all.png", Info = "{0}%".Format(phys_mit), Label = "Mitigation"});
+        else
+        {
+            if (phys_mit > 0) output.Add(new StatusIcon { FileName = "mit_phys.png", Info = "{0}%".Format(phys_mit), Label = "Physical Mit" });
+            if (magi_mit > 0) output.Add(new StatusIcon { FileName = "mit_all.png", Info = "{0}%".Format(magi_mit), Label = "Magical Mit" });
+        }
+
+        // Damage Up
+        if (phys_up == magi_up && phys_up > 0) output.Add(new StatusIcon { FileName = "all_up.png", Info = "{0}%".Format(phys_mit), Label = "Damage Up"});
+        else
+        {
+            if (phys_up > 0) output.Add(new StatusIcon { FileName = "phys_up.png", Info = "{0}%".Format(phys_mit), Label = "Phyiscal Dmg Up" });
+            if (magi_up > 0) output.Add(new StatusIcon { FileName = "magi_up.png", Info = "{0}%".Format(phys_mit), Label = "Magical Dmg Up" });
+        }
+
+        // Send Message to log for status effects that are missing
+        var debugMessage = "";
+        foreach (var item in debug)
+        {
+            if (!missing_ids.Contains(item))
+            {
+                debugMessage += "{0} = {1}; ".Format(item.Item1, item.Item2);
+                missing_ids.Add(item);
+            }
+        }
+        if (debugMessage.Length > 0) PluginLog.Debug("Missing Status Ids: " + debugMessage);
+
+        return output;
+    }
+
+    // Replace $mit_all.png$ with an image when rendering
+    private void DrawStatusIcons(List<StatusIcon> icons)
+    {
+
+        if (this.Size == null) return;
+        var width = ((Vector2)this.Size).X;
+        ImGui.SetCursorPosX(width);
+        ImGui.Text(""); // Empty to make .Sameline work nicer
+
+        // TODO: the SetCursorPosX calls are scuffed, work out actual widths
+        // just don't question the scalars
+        foreach (var icon in icons)
+        {
+            if (icon.Label != null)
+            {
+                ImGui.SetCursorPosX(ImGui.GetCursorPosX() - (1.5f * ImGui.CalcTextSize(icon.Info).X));
+                ImGui.SameLine();
+                ImGui.Text(icon.Label);
+                ImGui.SetCursorPosX(ImGui.GetCursorPosX() - (1.5f * ImGui.CalcTextSize(icon.Info).X));
+            }
+            PluginLog.Debug("textures: {0}", string.Join(", ", plugin.textures));
+            ImGui.SetCursorPosX(ImGui.GetCursorPosX() - (3 * ImGui.GetFontSize()));
+            ImGui.SameLine();
+            ImGui.Image(plugin.textures[icon.FileName].ImGuiHandle, new Vector2(ImGui.GetFontSize(), ImGui.GetFontSize()));
+            ImGui.SetCursorPosX(ImGui.GetCursorPosX() - (3 * ImGui.GetFontSize()));
+
+            if (icon.Info != null) {
+                ImGui.SetCursorPosX(ImGui.GetCursorPosX() - (1.5f * ImGui.CalcTextSize(icon.Info).X));
+                ImGui.SameLine();
+                ImGui.Text(icon.Info);
+                ImGui.SetCursorPosX(ImGui.GetCursorPosX() - (1.5f * ImGui.CalcTextSize(icon.Info).X));
+            }
+        }
+        //ImGui.PopItemWidth();
+    }
+
+    // VS might moan about some of these being possibly Null.
+    // VS is wrong, we just filtered out the Null values.
+    internal bool one_true(IEnumerable<bool?> values)
+    {
+        // filter out null values then aggregate using or
+        return values.Where(x => x != null).Aggregate(false, (a, b) => a || b.Value);
+    }
+
+    internal float multi_sum(IEnumerable<float?> values)
+    {
+        // filter out null values then do the multiplicative application
+        var x = values.Where(x => x != null).Select(x => 1 - x).Aggregate(1f, (a, b) => a * b.Value);
+        return to_percent(1f - x);
+    }
+
+    internal int to_percent(float value)
+    {
+        return (int)Math.Round(value * 100, 0);
+    }
+
+    internal struct StatusIcon
+    {
+        public string FileName { get; set; } // The icon itself, e.g. "mit_all.png"
+        public string? Label { get; set; } // Static label on the icon, e.g. "Mitigation"
+        public string? Info { get; set; } // Info on the icon, e.g. the actual mit percentage
+    }
+}
